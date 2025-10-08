f2ce071ffbad613fdb7a2908225fe9be0ffca361
Chedly Bahles <medchedly.bahles@esprit.tn>
2025-10-08 11:43:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ permet à Vercel de bien gérer les chemins relatifs
+  build: {
+    outDir: 'dist', // ✅ dossier de sortie pour le build
+  },
 })
